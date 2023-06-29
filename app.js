e251c1f92a28fe21ae2355680ab6eff961ee3511
Samantha <samantha.fenton@live.co.uk>
2023-06-29 15:51:00 +0100

    created a function for adding new clients in app.js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 "use strict";
 console.log("strict");
+
+const clientForm = document.getElementById("clientForm");
+const clientList = document.getElementById("clientList");
+
+//create new instanc eof client with cf
+
+const allClients = [];
+
+function Client(name) {
+  this.name = name;
+  this.render = function () {
+    const listItem = document.createElement("li");
+    listItem.textContent = this.name;
+    clientList.appendChild(listItem);
+  };
+}
